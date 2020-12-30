150f02d6e69f9287a5435a00f74b3cd0d1013ce1
“Javier <frjavaro87@gmail.com>
2020-12-30 00:07:48 -0600

    int

--- 002.py
+++ 002.py
@@ -5,28 +5,16 @@ a = 23
 b = 23
 c = 23
 d = 20
-
 print(a + b * c + d)
-
 print("intentando aprender más de python")
 print("de madrugada, para no perder tiempo")
-
 print("014 180 606 171 789 243")
-
 from math import pi
-
-x = 2 * pi
-
-print(x)
-
-print(pi * 2)
-
-y = input(int())
-
-print(y)
-
-z = input(int())
-
-d = int(y) * int(z)
-
-print(d)
+    x = 2 * pi
+    print(x)
+    print(pi * 2)
+    y = input(int())
+    print(y)
+    z = input(int())
+    d = int(y) * int(z)
+    print(d)
