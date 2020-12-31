ee46a1ea8976aa2476148a847025d635444f7b08
“Javier <frjavaro87@gmail.com>
2020-12-30 21:19:29 -0600

    menos

--- 002.py
+++ 002.py
@@ -8,7 +8,6 @@ d = 20
 print(a + b * c + d)
 print("intentando aprender más de python")
 print("de madrugada, para no perder tiempo")
-print("014 180 606 171 789 243")
 from math import pi
 x = 2 * pi
 print(x)
