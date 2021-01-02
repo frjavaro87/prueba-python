4e68394106718b3aead31da3d0ca74add3c1a154
“Javier <frjavaro87@gmail.com>
2021-01-01 20:52:53 -0600

    if bueno

--- 001.py
+++ 001.py
@@ -12,23 +12,12 @@ print(c)
 
 print("escribe un número")
 
-#a = input()
+a = int(input())
 
-numero = int(raw_input("\nIngresa un número entero, por favor: "))
 
-if numero < 0:
-    numero = 0
-    print 'El número ingresado es negativo cambiado a cero.\n'
-elif numero == 0:
-    print 'El número ingresado es 0.\n'
-elif numero == 1:
-    print 'El número ingresado es 1.\n'
-else:
-    print 'El número ingresado es mayor que uno.\n'
-
-#if a <= 10:
- #   print("bien hecho, sigue así")
-#elif
- #   print("mala respuesta")
+if a <= 10:
+   print("bien hecho, sigue así")
+elif a > 10:
+   print("mala respuesta")
 
 
